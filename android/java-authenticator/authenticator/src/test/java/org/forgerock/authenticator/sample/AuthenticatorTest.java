b88f2a0ea1d5f5e94c3928881a56552ee923edcd
Stoyan Petrov <stoyan@petrov.ca>
2025-01-03 10:36:52 -0800

    Add simple test

--- android/java-authenticator/authenticator/src/test/java/org/forgerock/authenticator/sample/AuthenticatorTest.java
+++ android/java-authenticator/authenticator/src/test/java/org/forgerock/authenticator/sample/AuthenticatorTest.java
@@ -1,4 +1,14 @@
+// File: src/test/java/org/forgerock/authenticator/sample/AuthenticatorTest.java
+
 package org.forgerock.authenticator.sample;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class AuthenticatorTest {
+
+    @Test
+    public void testAuthenticate() {
+        assertEquals(4, 2 + 2);
+    }
 }
